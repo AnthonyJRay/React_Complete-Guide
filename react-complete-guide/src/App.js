8eb63f3e62b41ec484ca5d2c3fcc39c0c3d54701
Anthony Eriksen <ajrayid@gmail.com>
2020-01-04 15:40:03 -0700

    Notes, Understanding and using Props, and props.children

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -8,11 +8,11 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App.</h1>
         <p> This is really working!</p>
-        <Person />
-        <Person />
-        <Person />
-        <Person />
-        <Person />
+        <Person name="Anthony" age="30" />
+        <Person name="Debra" age="29" />
+        <Person name="Vincent" age="1" />
+        <Person name="Max" age="28" />
+        <Person name="Manu" age="29">My Hobbies: Racing</Person>
       </div>
     );
 

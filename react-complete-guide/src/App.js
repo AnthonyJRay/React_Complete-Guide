10b2ae03bae45233d7414bda2db2b8bd64cb8382
Anthony Eriksen <ajrayid@gmail.com>
2020-01-04 22:56:00 -0700

    Notes, Managing state in functional components with React Hooks.

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  state = {
+const App = props => {
+  useState({
     persons: [
       { name: 'Anthony', age: 30 },
       { name: 'Debra', age: 29 },
@@ -11,46 +11,42 @@ class App extends Component {
       { name: 'Max', age: 28 },
       { name: 'Manu', age: 29 }
     ]
-  }
-
-  switchNameHandler = () => {
-    // console.log('Was Clicked!');
-    this.setState({
-      persons: [
-        { name: 'Anthony', age: 50 },
-        { name: 'Debra', age: 25 },
-        { name: 'Vincent', age: 6 },
-        { name: 'Max', age: 100 },
-        { name: 'Manu', age: 23 }
-      ]
-    });
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi, I'm a React App.</h1>
-        <p> This is really working!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
-        <Person name={this.state.persons[3].name} age={this.state.persons[3].age} />
-        <Person name={this.state.persons[4].name} age={this.state.persons[4].age}>My Hobbies: Racing</Person>
-      </div>
-    );
-
-    // return React.createElement(
-    //   'div',
-    //   { className: 'App' },
-    //   React.createElement(
-    //     'h1',
-    //     null,
-    //     'Hi, I\'m a React App!'
-    //   )
-    // );
-
-  }
+  });
 }
+return (
+  <div className="App">
+    <h1>Hi, I'm a React App.</h1>
+    <p> This is really working!</p>
+    <button onClick={this.switchNameHandler}>Switch Name</button>
+    <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+    <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+    <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+    <Person name={this.state.persons[3].name} age={this.state.persons[3].age} />
+    <Person name={this.state.persons[4].name} age={this.state.persons[4].age}>My Hobbies: Racing</Person>
+  </div>
+);
 
 export default App;
+
+state = {
+  persons: [
+    { name: 'Anthony', age: 30 },
+    { name: 'Debra', age: 29 },
+    { name: 'Vincent', age: 1 },
+    { name: 'Max', age: 28 },
+    { name: 'Manu', age: 29 }
+  ]
+}
+
+switchNameHandler = () => {
+  // console.log('Was Clicked!');
+  this.setState({
+    persons: [
+      { name: 'Anthony', age: 50 },
+      { name: 'Debra', age: 25 },
+      { name: 'Vincent', age: 6 },
+      { name: 'Max', age: 100 },
+      { name: 'Manu', age: 23 }
+    ]
+  });
+};

1e4638657a3c1069a960804ec105d8ca54c628de
Anthony Eriksen <ajrayid@gmail.com>
2020-01-10 11:38:40 -0700

    Notes, Flexible Lists, Wrap Up

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -7,11 +7,11 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     persons: [
-      { name: 'Anthony', age: 30 },
-      { name: 'Debra', age: 29 },
-      { name: 'Vincent', age: 1 },
-      { name: 'Max', age: 26 },
-      { name: 'Manu', age: 21 }
+      { id: '0', name: 'Anthony', age: 30 },
+      { id: '1', name: 'Debra', age: 29 },
+      { id: '2', name: 'Vincent', age: 1 },
+      { id: '3', name: 'Max', age: 26 },
+      { id: '4', name: 'Manu', age: 21 }
     ],
     otherState: 'some other value',
     showPersons: false
@@ -34,16 +34,21 @@ class App extends Component {
 
 
   //  NAME CHANGE
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-        { name: 'Anthony', age: 50 },
-        { name: event.target.value, age: 25 },
-        { name: 'Vincent', age: 6 },
-        { name: 'Max', age: 100 },
-        { name: 'Manu', age: 23 }
-      ]
-    })
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    }
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({ persons: persons })
   }
 
 
@@ -83,6 +88,8 @@ class App extends Component {
               click={() => this.deletePersonHandler(index)}
               name={person.name}
               age={person.age}
+              key={person.id}
+              changed={(event) => this.nameChangedHandler(event, person.id)}
             />
           })}
         </div>

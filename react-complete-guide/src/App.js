4bd0e5d08f53c0eac8228efebe5b5d859eb5c418
Anthony Eriksen <ajrayid@gmail.com>
2020-01-11 18:33:41 -0700

    Adding styles dynamically

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -106,12 +106,17 @@ class App extends Component {
   render() {
 
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
       cursor: 'pointer'
     };
+
+    const inputStyle = {
+      marginTop: '350px'
+    }
 
 
     // CONDITIONALS
@@ -131,6 +136,16 @@ class App extends Component {
           })}
         </div>
       );
+
+      style.backgroundColor = 'red';
+    }
+
+    const classes = []
+    if (this.state.persons.length <= 2) {
+      classes.push('red'); // classes = ['red']
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push('bold'); // class = ['red', 'bold']
     }
 
     // JSX / COMPONENTS
@@ -138,7 +153,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hi, I'm a React App.</h1>
-        <p> This is really working!</p>
+        <p className={classes.join(' ')}> This is really working!</p>
         <button
           style={style}
           onClick={this.togglePersonsHandler}>Toggle Persons</button>
@@ -147,6 +162,7 @@ class App extends Component {
         <input type="text"
           onChange={this.inputChangeHandler}
           value={this.props.stringState}
+          style={inputStyle}
         />
         <div>
           {this.state.stringState}
